757a0a1bfdcafe0536896ec036ef05e0ccc130d2
bohutahennadii <113023459+bohutahennadii@users.noreply.github.com>
2024-03-01 19:26:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ def check_dict(text):
                 nums += 1
         
         result.append(nums)
-    st.subheader(result)
+    
     max_ind = result.index(max(result))
     return dict[max_ind]
 
